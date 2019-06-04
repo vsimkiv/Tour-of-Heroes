b0d016bb8db772fa60a3f2d1c1a1db359459293b
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2019-06-04 16:02:05 +0300

    create HeroDetailsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hero-details/hero-details.component.ts
@@ -0,0 +1,17 @@
+import { Hero } from './../../models/Hero';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css']
+})
+export class HeroDetailsComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
